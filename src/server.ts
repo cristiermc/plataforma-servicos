9f9a31d562f26edea74f644f746ed7cd28fab5ff
Cristier Cittadin <cristiermc@gmail.com>
2026-09-15 11:16:49 -0300

    feat: add basic HTML server and routes

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,24 @@
-console.log("Server is starting...");
+import {createServer} from "node:http";
+
+const server = createServer((request, response) => {
+    if (request.url === "/") {
+        response.statusCode = 200;
+        response.setHeader("Content-Type", "text/plain");
+        response.end("FixFast Funcionando!");
+        return;
+    }
+    if (request.url === "/health") {
+        response.statusCode = 200;
+        response.setHeader("Content-Type", "application/json; charset=utf-8");
+        response.end(JSON.stringify({status: "ok"}));
+        return;
+    }
+
+    response.statusCode = 404;
+    response.setHeader("Content-Type", "application/json");
+    response.end(JSON.stringify({error: "Rota não encontrada"}));
+});
+
+server.listen(3000, () => {
+    console.log("Server rodando em http://localhost:3000");
+});
